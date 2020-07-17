2cf0e289da7cfb636d03c78a18f96b53567fa417
阿小庆 <1756260160@qq.com>
2020-07-17 10:04:15 +0800

    api

--- src/api/test.js
+++ src/api/test.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function testList() {
   return request({
-    url: '/api/4/news/latest',
+    url: '/api/3/news/latest',
     method: 'get'
     // baseURL: 'http://news.at.zhihu.com' // 直接通过覆盖的方式
   })
