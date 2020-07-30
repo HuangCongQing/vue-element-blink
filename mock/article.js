ddd3dbe6982444098a5fa69d18737f5c8dd4df84
阿小庆 <1756260160@qq.com>
2020-07-31 03:41:05 +0800

    enhance：磁瓦data

--- mock/article.js
+++ mock/article.js
@@ -1,32 +1,453 @@
 const Mock = require('mockjs')
 
-const List = []
-const count = 100
+// const List = []
+// const count = 100
 
 const baseContent = '<p>I am testing data, I am testing data.</p><p><img src="https://wpimg.wallstcn.com/4c69009c-0fd4-4153-b112-6cb53d1cf943"></p>'
 const image_uri = 'https://wpimg.wallstcn.com/e4558086-631c-425c-9430-56ffb46e70b3'
 
-for (let i = 0; i < count; i++) {
-  List.push(Mock.mock({
-    id: '@increment',
-    timestamp: +Mock.Random.date('T'),
-    author: '@first',
+// for (let i = 0; i < count; i++) {
+//   List.push(Mock.mock({
+//     id: '@increment',
+//     timestamp: +Mock.Random.date('T'),
+//     author: '@first',
+//     reviewer: '@first',
+//     title: '@title(5, 10)',
+//     content_short: 'mock data',
+//     content: baseContent,
+//     forecast: '@float(0, 100, 2, 2)',
+//     importance: '@integer(1, 3)',
+//     'type|1': ['CN', 'US', 'JP', 'EU'],
+//     'status|1': ['published', 'draft'],
+//     display_time: '@datetime',
+//     comment_disabled: true,
+//     pageviews: '@integer(300, 5000)',
+//     image_uri,
+//     platforms: ['a-platform']
+//   }))
+// }
+const List = [
+  Mock.mock({
+    id: '1',
+    timestamp: '1591015491',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
     reviewer: '@first',
-    title: '@title(5, 10)',
     content_short: 'mock data',
     content: baseContent,
     forecast: '@float(0, 100, 2, 2)',
-    importance: '@integer(1, 3)',
     'type|1': ['CN', 'US', 'JP', 'EU'],
     'status|1': ['published', 'draft'],
     display_time: '@datetime',
     comment_disabled: true,
-    pageviews: '@integer(300, 5000)',
     image_uri,
     platforms: ['a-platform']
-  }))
-}
-
+  }),
+  Mock.mock({
+    id: '2',
+    timestamp: '1591015666',
+    title: 'A', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '3',
+    timestamp: '1591015777',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '4',
+    timestamp: '1591015888',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '5',
+    timestamp: '1591015999',
+    title: 'B', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '气孔', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '外弧面', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 0, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '6',
+    timestamp: '1591016000',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '7',
+    timestamp: '1591016111',
+    title: 'A', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '8',
+    timestamp: '1591016222',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '9',
+    timestamp: '1591016222',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '凸点', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '崩缺', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '10',
+    timestamp: '1591016222',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '11',
+    timestamp: '1591016222',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '12',
+    timestamp: '1591016333',
+    title: 'A', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '13',
+    timestamp: '1591016444',
+    title: 'B', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '14',
+    timestamp: '1591016555',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '15',
+    timestamp: '1591016666',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '16',
+    timestamp: '1591016788',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  ['0', '外弧面', '纵切面', '顶角', '内弧面', '端面'],
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '17',
+    timestamp: '1591016888',
+    title: 'B', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '18',
+    timestamp: '1591016999',
+    title: 'A', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '19',
+    timestamp: '1591017111',
+    title: 'C', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '20',
+    timestamp: '1591017222',
+    title: 'D', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  }),
+  Mock.mock({
+    id: '21',
+    timestamp: '1591017333',
+    title: 'A', // 3  CiwaType  查询
+    author: 'D001', // 4 MachineNum
+    importance: 'SB2020B006', // 5 MaterialNum
+    pageviews: '0', // Readings 6 DefectType  查询 ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶']
+    DefectPos: '0', // 7 DefectPos  查询  0 外弧面 纵切面  顶角  内弧面 端面
+    Accepted: 1, // 8 Accepted 1
+    reviewer: '@first',
+    content_short: 'mock data',
+    content: baseContent,
+    forecast: '@float(0, 100, 2, 2)',
+    'type|1': ['CN', 'US', 'JP', 'EU'],
+    'status|1': ['published', 'draft'],
+    display_time: '@datetime',
+    comment_disabled: true,
+    image_uri,
+    platforms: ['a-platform']
+  })
+]
 module.exports = [
   {
     url: '/vue-element-admin/article/list', //  文章列表
--- src/views/table/complex-table.vue
+++ src/views/table/complex-table.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="Title" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.importance" placeholder="Imp" clearable style="width: 90px" class="filter-item">
+      <el-input v-model="listQuery.title" placeholder="CiwaType" style="width: 100px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-select v-model="listQuery.pageviews" placeholder="DefectType" clearable style="width: 140px" class="filter-item">
         <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
       </el-select>
-      <el-select v-model="listQuery.type" placeholder="Type" clearable class="filter-item" style="width: 130px">
-        <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name+'('+item.key+')'" :value="item.key" />
+      <el-select v-model="listQuery.DefectPos" placeholder="DefectPos" clearable class="filter-item" style="width: 130px">
+        <el-option v-for="item in DefectPosOptions" :key="item" :label="item" :value="item" />
       </el-select>
       <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
         <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
@@ -14,9 +14,9 @@
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         Search
       </el-button>
-      <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
-        Add
-      </el-button>
+      <!-- <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
+        Add（待开发）
+      </el-button> -->
       <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
         Export
       </el-button>
@@ -40,18 +40,17 @@
           <span>{{ row.id }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Date" width="150px" align="center">
+      <el-table-column label="Date" width="200px" align="center">
         <template slot-scope="{row}">
           <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Title" min-width="150px">
+      <el-table-column label="CiwaType" width="100px" align="center">
         <template slot-scope="{row}">
-          <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
-          <el-tag>{{ row.type | typeFilter }}</el-tag>
+          <span class="link-type">{{ row.title }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110px" align="center">
+      <el-table-column label="MachineNum" width="150px" align="center">
         <template slot-scope="{row}">
           <span>{{ row.author }}</span>
         </template>
@@ -61,38 +60,29 @@
           <span style="color:red;">{{ row.reviewer }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Imp" width="80px">
+      <el-table-column label="MaterialNum" width="190px" align="center">
         <template slot-scope="{row}">
-          <svg-icon v-for="n in + row.importance" :key="n" icon-class="star" class="meta-item__icon" />
+          <span>{{ row.importance }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Readings" align="center" width="95">
+      <el-table-column label="DefectType" align="center" width="120">
         <template slot-scope="{row}">
           <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
           <span v-else>0</span>
         </template>
       </el-table-column>
-      <el-table-column label="Status" class-name="status-col" width="100">
+      <el-table-column label="DefectPos" class-name="status-col" width="100">
         <template slot-scope="{row}">
-          <el-tag :type="row.status | statusFilter">
-            {{ row.status }}
+          <el-tag>
+            {{ row.DefectPos }}
           </el-tag>
         </template>
       </el-table-column>
-      <el-table-column label="Actions" align="center" width="230" class-name="small-padding fixed-width">
-        <template slot-scope="{row,$index}">
-          <el-button type="primary" size="mini" @click="handleUpdate(row)">
-            Edit
-          </el-button>
-          <el-button v-if="row.status!='published'" size="mini" type="success" @click="handleModifyStatus(row,'published')">
-            Publish
-          </el-button>
-          <el-button v-if="row.status!='draft'" size="mini" @click="handleModifyStatus(row,'draft')">
-            Draft
-          </el-button>
-          <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
-            Delete
-          </el-button>
+      <el-table-column label="Accepted" align="center" width="130" class-name="small-padding fixed-width">
+        <template slot-scope="{row}">
+          <el-tag :type="row.Accepted ">
+            {{ row.Accepted }}
+          </el-tag>
         </template>
       </el-table-column>
     </el-table>
@@ -192,12 +182,13 @@ export default {
       listQuery: {
         page: 1,
         limit: 20,
-        importance: undefined,
+        pageviews: undefined,
         title: undefined,
         type: undefined,
         sort: '+id'
       },
-      importanceOptions: [1, 2, 3],
+      importanceOptions: ['0', '崩缺', '气孔', '凸点', '欠磨', '裂纹', '结晶'], //  DefectType  0  崩缺 气孔 凸点 欠磨 裂纹 结晶
+      DefectPosOptions: ['0', '外弧面', '纵切面', '顶角', '内弧面', '端面'], // 0 外弧面 纵切面  顶角  内弧面 端面
       calendarTypeOptions,
       sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
       statusOptions: ['published', 'draft', 'deleted'],
@@ -353,8 +344,8 @@ export default {
     handleDownload() {
       this.downloadLoading = true
       import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
-        const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
+        const tHeader = ['timestamp', 'CiwaType', 'MachineNum', 'MaterialNum', 'DefectType', 'DefectPos', 'Accepted'] // 对应接口数据名字
+        const filterVal = ['timestamp', 'title', 'author', 'importance', 'pageviews', 'DefectPos', 'Accepted']
         const data = this.formatJson(filterVal)
         excel.export_json_to_excel({
           header: tHeader,
